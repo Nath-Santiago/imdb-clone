15ed1bb70c49d89b47c4ed2df4769fbd2e55966b
Nath-Santiago <nathaniel.santiago@98labs.com>
2024-04-04 10:31:54 +0800

    Added Movies Page

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,12 @@
 const nextConfig = {
     images: {
         remotePatterns: [
-          {
+        // https://image.tmdb.org/t/p/...
+          { 
             protocol: 'https',
             hostname: 'image.tmdb.org',
             port: '',
-            pathname: '/t/p/w500/**',
+            pathname: '/t/p/**',
           },
         ],
       },
